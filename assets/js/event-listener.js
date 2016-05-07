e8189cda5001d5e2af55bcd773d2e1cb404e72d9
Sahil Dua <sahildua2305@gmail.com>
2016-05-08 01:37:49 +0530

    Add changing button text feature upon saving

--- assets/js/event-listener.js
+++ assets/js/event-listener.js
@@ -2,7 +2,7 @@
 * @Author: sahildua2305
 * @Date:   2016-05-02 02:10:18
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-04 03:46:16
+* @Last Modified time: 2016-05-04 04:23:39
 */
 
 $(document).on('ready', function(){
@@ -30,6 +30,8 @@ $(document).on('ready', function(){
 	$("#save_links").on('click', function(e){
 		console.log("Request for saving links in storage");
 
+		document.getElementById("save_links").innerHTML = "Saving..";
+
 		for(var i in list_keys){
 			var key = list_keys[i];
 			var newValue = document.getElementById(key).value;
@@ -41,6 +43,8 @@ $(document).on('ready', function(){
 
 		// trigger to show the saved links
 		updateUI();
+
+		document.getElementById("save_links").innerHTML = "Save links";
 	});
 
 	// trigger to show the saved links
