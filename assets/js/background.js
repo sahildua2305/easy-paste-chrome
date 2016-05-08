dae43616a343a9669860e86e20e09231ea6fcca3
paulxuca <paulxuca@gmail.com>
2016-05-08 15:13:16 -0400

    Added auto-fill ability based on user input

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,88 +1,99 @@
 /*
-* @Author: sahildua2305
-* @Date:   2016-05-01 07:29:02
-* @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-08 04:21:43
-*/
+ * @Author: sahildua2305
+ * @Date:   2016-05-01 07:29:02
+ * @Last Modified by:   Sahil Dua
+ * @Last Modified time: 2016-05-08 04:21:43
+ */
 
 
 var parent = chrome.contextMenus.create({
-	"title": "Paste easy link here",
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "Paste easy link here",
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
 var linkedin = chrome.contextMenus.create({
-	"title": "LinkedIn",
-	"parentId": parent,
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "LinkedIn",
+    "parentId": parent,
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
 var github = chrome.contextMenus.create({
-	"title": "GitHub",
-	"parentId": parent,
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "GitHub",
+    "parentId": parent,
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
 var twitter = chrome.contextMenus.create({
-	"title": "Twitter",
-	"parentId": parent,
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "Twitter",
+    "parentId": parent,
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
 var facebook = chrome.contextMenus.create({
-	"title": "Facebook",
-	"parentId": parent,
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "Facebook",
+    "parentId": parent,
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
 var personal = chrome.contextMenus.create({
-	"title": "Personal Website",
-	"parentId": parent,
-	"contexts": ["editable"],
-	"onclick": pasteLink
+    "title": "Personal Website",
+    "parentId": parent,
+    "contexts": ["editable"],
+    "onclick": pasteLink
 });
 
-function pasteLink(info, tab){
 
-	var requested_key, requested_link, status;
+function pasteLink(info, tab) {
 
-	if(info.menuItemId == linkedin){
-		requested_key = "linkedin";
-	}
-	else if(info.menuItemId == github){
-		requested_key = "github";
-	}
-	else if(info.menuItemId == twitter){
-		requested_key = "twitter";
-	}
-	else if(info.menuItemId == facebook){
-		requested_key = "facebook";
-	}
-	else if(info.menuItemId == personal){
-		requested_key = "personal";
-	}
+    var requested_key, requested_link, status;
 
-	//Add all you functional Logic here
-	chrome.tabs.query({
-		"active": true,
-		"currentWindow": true
-	}, function (tabs) {
-		requested_link = localStorage.getItem(requested_key);
-		status = "success";
-		if(typeof requested_link == "object" || requested_link == ""){
-			status = "fail";
-		}
+    if (info.menuItemId == linkedin) {
+        requested_key = "linkedin";
+    } else if (info.menuItemId == github) {
+        requested_key = "github";
+    } else if (info.menuItemId == twitter) {
+        requested_key = "twitter";
+    } else if (info.menuItemId == facebook) {
+        requested_key = "facebook";
+    } else if (info.menuItemId == personal) {
+        requested_key = "personal";
+    }
 
-		// send message to the current tab
-		chrome.tabs.sendMessage(tabs[0].id, {
-			status: status,
-			link: requested_link,
-			type: typeof requested_link
-		});
-	});
+    //Add all you functional Logic here
+    chrome.tabs.query({
+        "active": true,
+        "currentWindow": true
+    }, function(tabs) {
+        requested_link = localStorage.getItem(requested_key);
+        status = "success";
+        if (typeof requested_link == "object" || requested_link == "") {
+            status = "fail";
+        }
+
+        // send message to the current tab
+        chrome.tabs.sendMessage(tabs[0].id, {
+            status: status,
+            link: requested_link,
+            type: typeof requested_link
+        });
+    });
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	var allLinks = {};
+
+	for(var key in localStorage){
+		if(localStorage.hasOwnProperty(key) && localStorage.getItem(key) !== ''){
+			allLinks[key] = localStorage.getItem(key);
+		}
+	}
+    if (request.method == "getLocalStorage")
+        sendResponse({ data: allLinks });
+    else
+        sendResponse({}); // snub them.
+});
