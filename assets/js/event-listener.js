d1759ae79bc56713270d77bd951671cce1cadc53
Arpit Gogia <arpitgogia@protonmail.com>
2016-07-30 12:20:11 +0530

    Saving Custom Links fixed

--- assets/js/event-listener.js
+++ assets/js/event-listener.js
@@ -16,8 +16,11 @@ $(document).on('ready', function(){
 	function updateUI(flag){
 		//If not called from saved_links, we probably need to add html for the custom fields already in storage.
 		if(flag == true) {
-			for(var i = 0 ; i < custom_count ; i++) 
-				$('#custom').append('<div class="form-group"><label for="custom" class="col-md-2 col-sm-2 control-label">Custom Link</label><div class="col-md-8 col-sm-8"><input type="text" class="form-control" id="custom' + i.toString() + '" placeholder="Custom Link"></div></div>')
+			for(var i = 0 ; i < custom_count ; i++) {
+				var id = 'custom' + i.toString();
+				var html = "<div class=\"form-group\"><div class=\"col-md-8 col-sm-8\"><div class=\"input-group\"><div class=\"input-group-addon\"><i class=\"fa fa-external-link-square\"></i></div><input type=\"text\" class=\"form-control\" id=" + id + " placeholder=\"Custom link\"></div></div></div>"
+				$('#custom').append(html);			
+			}
 			for(var i = 0 ; i < custom_count ; i++)
 				list_keys.push('custom' + i.toString());
 		}
@@ -26,6 +29,7 @@ $(document).on('ready', function(){
 		for(var i in list_keys){
 			var key = list_keys[i];
 			var link = localStorage.getItem(key);
+			console.log(key + " : " + typeof link);
 			if(typeof link == "string"){
 				document.getElementById(key).value = link;
 			}
@@ -42,7 +46,9 @@ $(document).on('ready', function(){
 
 		for(var i in list_keys){
 			var key = list_keys[i];
+			console.log(key);
 			var newValue = document.getElementById(key).value;
+			console.log(key + ' : ' + newValue);
 			localStorage.setItem(key, newValue);
 		}
 
@@ -58,18 +64,10 @@ $(document).on('ready', function(){
 	 * Currently once you press add link, it will thus be appended and even if empty will show up in the list again.
 	 */
 	$("#add_link").on('click', function(e) {
-		/*
-		<div class="form-group">
-					<div class="col-md-8 col-sm-8">
-						<div class="input-group">
-							<div class="input-group-addon"><i class="fa fa-globe"></i></div>
-							<input type="text" class="form-control" id="personal" placeholder="Personal website link">
-						</div>
-					</div>
-				</div>
-		*/
-		$('#custom').append('<div class="form-group"><div class="col-md-8 col-sm-8"><div class="input-group"><div class="input-group-addon"><i class="fa fa-external-link-square"></i></div><input type="text" class="form-control" id="' + custom_count.toString() + '" placeholder="Custom link"></div></div></div>')
-		list_keys.push('custom' + custom_count.toString());
+		var id = 'custom' + custom_count.toString()
+		var html = "<div class=\"form-group\"><div class=\"col-md-8 col-sm-8\"><div class=\"input-group\"><div class=\"input-group-addon\"><i class=\"fa fa-external-link-square\"></i></div><input type=\"text\" class=\"form-control\" id=" + id + " placeholder=\"Custom link\"></div></div></div>"
+		$('#custom').append(html);
+		list_keys.push("custom" + custom_count.toString());
 		custom_count += 1;
 		localStorage.setItem('custom_count', custom_count);
 	});
